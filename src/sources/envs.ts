0b68d7277d22499dbd3129dd70bc1f278de3f0db
Victor <victor.mendele68@gmail.com>
2025-08-14 13:56:13 +0200

    refactor the envs source

--- src/sources/envs.ts
+++ src/sources/envs.ts
@@ -26,75 +26,84 @@ export type EnvSourceOpts = {
   loadSecrets?: boolean
 }
 
-export function envSource (opts: EnvSourceOpts = {}): Source<"envs", NodeJS.ProcessEnv> {
-  const { prefix = "", loadSecrets = false } = opts
+class EnvSource implements Source<"envs", NodeJS.ProcessEnv> {
+  key: "envs" = "envs" as const
 
-  return {
-    key: "envs",
-    load(schema: ObjectSchema<ObjectSpec>, loaded: Record<string, unknown>, envs: NodeJS.ProcessEnv = process.env) : Promise<unknown> {
-      const entries = flatten(schema)
+  #opts: Required<EnvSourceOpts>
 
-      const filteredEntries = !loadSecrets
-        ? entries.filter(e => !isSecret(e.value))
-        : entries
+  constructor(config: Required<EnvSourceOpts>) {
+    this.#opts = config
+  }
 
-      const config = {}
+  async load(schema: ObjectSchema<ObjectSpec>, loaded: Record<string, unknown>, envs: NodeJS.ProcessEnv = process.env): Promise<unknown> {
+    const entries = flatten(schema)
 
-      for (const entry of filteredEntries) {
-        const envAliases = entry.value._aliases.filter(a => a.sourceKey === "envs")
-        const defaultEnvKey = [prefix, entry.key.join("_").toUpperCase()].join("")
-        const allEnvKeys = [ defaultEnvKey, ...envAliases.map(a => a.id) ]
+    const filteredEntries = !this.#opts.loadSecrets
+      ? entries.filter(e => !isSecret(e.value))
+      : entries
 
-        let envValue = undefined
+    const config = {}
 
-        for (const envKey of allEnvKeys) {
-          if (envs[envKey] !== undefined) {
-            envValue = envs[envKey]
-            break
-          }
+    for (const entry of filteredEntries) {
+      const envAliases = entry.value._aliases.filter(a => a.sourceKey === "envs")
+      const defaultEnvKey = [this.#opts.prefix, entry.key.join("_").toUpperCase()].join("")
+      const allEnvKeys = [defaultEnvKey, ...envAliases.map(a => a.id)]
+
+      let envValue = undefined
+
+      for (const envKey of allEnvKeys) {
+        if (envs[envKey] !== undefined) {
+          envValue = envs[envKey]
+          break
         }
-
-        if (envValue === undefined) {
-          continue
-        }
-
-        let tmp = config
-        const intermediateObjectPath = entry.key.slice(0, -1)
-
-        // Asserts intermediate objects are created.
-        for (const chunk of intermediateObjectPath) {
-          if (!(chunk in tmp)) {
-            Object.defineProperty(tmp, chunk, {
-              value: {},
-              enumerable: true,
-              configurable: true,
-              writable: true
-            })
-          }
-          // @ts-expect-error
-          tmp = tmp[chunk]
-        }
-
-        const key = entry.key.at(-1)
-
-        if (key === undefined) {
-          throw new Error("key is undefined")
-        }
-
-        Object.defineProperty(tmp, key, {
-          value: entry.value.coerce?.(envValue) ?? envValue,
-          enumerable: true,
-          configurable: true,
-          writable: true
-        })
       }
 
-      return Promise.resolve(config)
+      if (envValue === undefined) {
+        continue
+      }
+
+      let tmp = config
+      const intermediateObjectPath = entry.key.slice(0, -1)
+
+      // Asserts intermediate objects are created.
+      for (const chunk of intermediateObjectPath) {
+        if (!(chunk in tmp)) {
+          Object.defineProperty(tmp, chunk, {
+            value: {},
+            enumerable: true,
+            configurable: true,
+            writable: true
+          })
+        }
+        // @ts-expect-error
+        tmp = tmp[chunk]
+      }
+
+      const key = entry.key.at(-1)
+
+      if (key === undefined) {
+        throw new Error("key is undefined")
+      }
+
+      Object.defineProperty(tmp, key, {
+        value: entry.value.coerce?.(envValue) ?? envValue,
+        enumerable: true,
+        configurable: true,
+        writable: true
+      })
     }
+
+    return Promise.resolve(config)
   }
 }
 
-export function envAlias (id: string): Alias {
+export function envSource(opts: EnvSourceOpts = {}): Source<"envs", NodeJS.ProcessEnv> {
+  const {prefix = "", loadSecrets = false} = opts
+
+  return new EnvSource({ prefix, loadSecrets })
+}
+
+export function envAlias(id: string): Alias {
   return {
     id,
     sourceKey: "envs"
