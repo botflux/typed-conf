7b003a404d5e0993dbcb00f6e69f737f5316d914
Victor <victor.mendele68@gmail.com>
2025-10-11 18:40:56 +0200

    test the float schema

---
+++ src/schemes/float.unit.ts
@@ -0,0 +1,66 @@
+import {describe, it} from "node:test";
+import {c} from "../loader.js";
+import {expect} from "expect";
+import {envAlias} from "../sources/envs.js";
+
+describe('float', function () {
+  it('should be able to declare a float', function () {
+    // Given
+    // When
+    const schema = c.float()
+
+    // Then
+    expect(schema.schema.schema).toEqual({
+      type: 'number'
+    })
+  })
+
+  it('should be able to declare an optional float', function () {
+    // Given
+    // When
+    const schema = c.float().optional()
+
+    // Then
+    expect(schema.schema.optional).toBe(true)
+  })
+
+  it('should be required by default', function () {
+    // Given
+    // When
+    const schema = c.float()
+
+    // Then
+    expect(schema.schema.optional).toBe(false)
+  })
+
+  it('should be able to declare aliases', function () {
+    // Given
+    // When
+    const schema = c.float().aliases(envAlias('FOO'))
+
+    // Then
+    expect(schema.schema.aliases).toEqual([envAlias('FOO')])
+  })
+
+  describe('coercion', function () {
+    const cases = [
+      [ 'foo', 'foo' ] as const,
+      [ '1', 1 ] as const,
+      [ '2.3', 2.3 ] as const,
+      [ false, false ] as const
+    ]
+
+    for (const [ input, expected ] of cases) {
+      it(`should be able to return ${expected} given ${input}`, function () {
+        // Given
+        const schema = c.float()
+
+        // When
+        const result = schema.schema.coerce?.(input)
+
+        // Then
+        expect(result).toBe(expected)
+      })
+    }
+  })
+})
