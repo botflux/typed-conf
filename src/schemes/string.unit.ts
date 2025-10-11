2e5959b352f01b76f308711b26e1632c0cd2b069
Victor <victor.mendele68@gmail.com>
2025-10-11 18:59:34 +0200

    test the string schema

---
+++ src/schemes/string.unit.ts
@@ -0,0 +1,44 @@
+import {describe, it} from "node:test";
+import {c} from "../loader.js";
+import {expect} from "expect";
+import {envAlias} from "../sources/envs.js";
+
+describe('string', function () {
+  it('should be able to declare a string schema', function () {
+    // Given
+    // When
+    const schema = c.string()
+
+    // Then
+    expect(schema.schema.schema).toEqual({
+      type: 'string',
+    })
+  })
+
+  it('should be able to declare an optional string', function () {
+    // Given
+    // When
+    const schema = c.string().optional()
+
+    // Then
+    expect(schema.schema.optional).toBe(true)
+  })
+
+  it('should be required by default', function () {
+    // Given
+    // When
+    const schema = c.string()
+
+    // Then
+    expect(schema.schema.optional).toBe(false)
+  })
+
+  it('should be able to declare aliases', function () {
+    // Given
+    // When
+    const schema = c.string().aliases(envAlias('FOO'))
+
+    // Then
+    expect(schema.schema.aliases).toEqual([ envAlias('FOO') ])
+  })
+})
