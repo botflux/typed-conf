6e911224240e29d25654558b431636b424a458e1
botflux <victor.mendele68@gmail.com>
2025-11-09 14:42:02 +0100

    add a base schema

--- src/schemes2/string.ts
+++ src/schemes2/string.ts
@@ -1,31 +1,62 @@
 import type {Alias} from "../schemes/base.js";
+import type {JSONSchema} from "json-schema-to-typescript";
 
 export const kType = Symbol('kType')
 
-export interface StringSchema<T> {
+export interface BaseSchema<T> {
   [kType]: T
-  type: 'string'
-  schema: {
-    type: 'string'
-  }
+  schema: JSONSchema
   optional: boolean
   aliases: Alias[]
   secret: boolean
 }
 
-export interface StringSchemaBuilder<T> {
-  plain: StringSchema<T>
+export interface BaseSchemaBuilder<T> {
+  plain: BaseSchema<T>
 
   optional(): StringSchemaBuilder<T | undefined>
   secret(): StringSchemaBuilder<T>
   aliases(...aliases: Alias[]): StringSchemaBuilder<T>
 }
 
-class StringBuilder<T> implements StringSchemaBuilder<T> {
+export class BaseBuilder<T> implements BaseSchemaBuilder<T> {
   plain: StringSchema<T>
 
   constructor(plain: StringSchema<T>) {
     this.plain = plain
+  }
+
+  optional(): StringSchemaBuilder<T | undefined> {
+    return new StringBuilder({
+      ...this.plain,
+      optional: true,
+    })
+  }
+
+  secret(): StringSchemaBuilder<T> {
+    return new StringBuilder({
+      ...this.plain,
+      secret: true,
+    })
+  }
+
+  aliases(...aliases: Alias[]): StringSchemaBuilder<T> {
+    return new StringBuilder({
+      ...this.plain,
+      aliases: [...this.plain.aliases, ...aliases],
+    })
+  }
+}
+
+export interface StringSchema<T> extends BaseSchema<T> {
+  type: 'string'
+}
+
+export interface StringSchemaBuilder<T> extends BaseSchemaBuilder<T> {}
+
+class StringBuilder<T> extends BaseBuilder<T> implements StringSchemaBuilder<T> {
+  constructor(plain: StringSchema<T>) {
+    super(plain)
   }
 
   optional(): StringSchemaBuilder<T | undefined> {
