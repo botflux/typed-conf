df2df480f44841588dfb6fbf3a00f022bd65389d
botflux <victor.mendele68@gmail.com>
2025-11-16 09:54:44 +0100

    move the env source in its own file

---
+++ src/sources2/env-source.ts
@@ -0,0 +1,76 @@
+import {isObject, type ObjectSchema} from "../schemes2/object.js";
+import type {BaseSchema} from "../schemes2/base.js";
+import {setValueAtPath} from "../utils.js";
+
+export type EnvSourceLoadOpts = {
+  envs?: NodeJS.ProcessEnv
+}
+
+class EnvSource {
+  async load(schema: ObjectSchema<Record<string, BaseSchema<unknown>>>, opts: EnvSourceLoadOpts) {
+    const {envs = process.env} = opts
+    const result: Record<string, unknown> = {}
+    const flattened = this.#flattenObjectSchema(schema)
+
+    const seenEnvNames = new Map<string, string>()
+
+    for (const [key, value] of flattened) {
+      const envName = camelCasePathToScreamingSnakeCase(key)
+      const mSeenEnvName = seenEnvNames.get(envName)
+      const envValue = envs[envName]
+
+      if (mSeenEnvName !== undefined) {
+        throw new Error(`The props ${mSeenEnvName} and ${key.join('.')} resolves to the same env ${envName}`)
+      }
+
+      seenEnvNames.set(envName, key.join('.'))
+
+      const coerced = value.coerce?.(envValue) ?? envValue
+
+      if (envs[envName] !== undefined) {
+        setValueAtPath(result, key, coerced)
+      }
+    }
+
+    return result
+  }
+
+  #flattenObjectSchema(schema: BaseSchema<unknown>, prefix: string[] = []): [path: string[], schema: BaseSchema<unknown>][] {
+    if (!isObject(schema)) {
+      return [[prefix, schema] as const]
+    }
+
+    return Object.entries(schema.props).flatMap(([key, schema]) => this.#flattenObjectSchema(
+      schema,
+      [...prefix, key]
+    ))
+  }
+}
+
+export function envSource() {
+  return new EnvSource()
+}
+
+function camelCasePathToScreamingSnakeCase(path: string[]) {
+  return path.map(camelCaseToScreamingSnakeCase).join('_')
+}
+
+export function camelCaseToScreamingSnakeCase(name: string) {
+  return name.split('').reduce(
+    (acc, current, index) => {
+      const next = name.at(index + 1)
+
+      if (next === undefined) {
+        return acc + current
+      }
+
+      const isCurrentLowercase = current.toLowerCase() === current
+      const isNextUppercase = next.toUpperCase() === next
+
+      if (isCurrentLowercase && isNextUppercase) {
+        return acc + current + '_'
+      }
+
+      return acc + current
+    }, '').toUpperCase()
+}
--- src/sources2/env-source.unit.ts
+++ src/sources2/env-source.unit.ts
@@ -1,59 +1,9 @@
 import {describe, it} from 'node:test'
-import {isObject, object, type ObjectSchema} from "../schemes2/object.js";
+import {object} from "../schemes2/object.js";
 import {string} from "../schemes2/string.js";
 import {expect} from "expect";
-import type {BaseSchema} from "../schemes2/base.js";
-import {setValueAtPath} from "../utils.js";
 import {integer} from "../schemes2/integer.js";
-
-export type EnvSourceLoadOpts = {
-  envs?: NodeJS.ProcessEnv
-}
-
-class EnvSource {
-  async load(schema: ObjectSchema<Record<string, BaseSchema<unknown>>>, opts: EnvSourceLoadOpts) {
-    const {envs = process.env} = opts
-    const result: Record<string, unknown> = {}
-    const flattened = this.#flattenObjectSchema(schema)
-
-    const seenEnvNames = new Map<string, string>()
-
-    for (const [key, value] of flattened) {
-      const envName = camelCasePathToScreamingSnakeCase(key)
-      const mSeenEnvName = seenEnvNames.get(envName)
-      const envValue = envs[envName]
-
-      if (mSeenEnvName !== undefined) {
-        throw new Error(`The props ${mSeenEnvName} and ${key.join('.')} resolves to the same env ${envName}`)
-      }
-
-      seenEnvNames.set(envName, key.join('.'))
-
-      const coerced = value.coerce?.(envValue) ?? envValue
-
-      if (envs[envName] !== undefined) {
-        setValueAtPath(result, key, coerced)
-      }
-    }
-
-    return result
-  }
-
-  #flattenObjectSchema(schema: BaseSchema<unknown>, prefix: string[] = []): [path: string[], schema: BaseSchema<unknown>][] {
-    if (!isObject(schema)) {
-      return [[prefix, schema] as const]
-    }
-
-    return Object.entries(schema.props).flatMap(([key, schema]) => this.#flattenObjectSchema(
-      schema,
-      [...prefix, key]
-    ))
-  }
-}
-
-function envSource() {
-  return new EnvSource()
-}
+import {camelCaseToScreamingSnakeCase, envSource} from "./env-source.js";
 
 describe('env source', function () {
   describe('#load', function () {
@@ -127,30 +77,6 @@ describe('env source', function () {
     })
   })
 })
-
-function camelCasePathToScreamingSnakeCase(path: string[]) {
-  return path.map(camelCaseToScreamingSnakeCase).join('_')
-}
-
-function camelCaseToScreamingSnakeCase(name: string) {
-  return name.split('').reduce(
-    (acc, current, index) => {
-      const next = name.at(index + 1)
-
-      if (next === undefined) {
-        return acc + current
-      }
-
-      const isCurrentLowercase = current.toLowerCase() === current
-      const isNextUppercase = next.toUpperCase() === next
-
-      if (isCurrentLowercase && isNextUppercase) {
-        return acc + current + '_'
-      }
-
-      return acc + current
-    }, '').toUpperCase()
-}
 
 describe('camelCaseToScreamingSnakeCase', function () {
   it('should be able to convert a simple word to screaming snake case', function () {
