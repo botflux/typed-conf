69239edfe4cdf004d72c6726d09eac1565ec4db0
botflux <victor.mendele68@gmail.com>
2025-12-07 22:01:41 +0100

    support describing a sub-config using the file ref

--- src/sources/file/schemes.ts
+++ src/sources/file/schemes.ts
@@ -1,16 +1,35 @@
 import {ref, type RefSchema} from "../../schemes/ref.js";
-import {string} from "../../schemes/string.js";
 import {any} from "../../schemes/any.js";
+import type {BaseSchema} from "../../schemes/base.js";
 
 export type EncodingToSchemaType<Encoding extends (BufferEncoding | undefined)> = Encoding extends BufferEncoding ? string : Buffer
+
+export type FileOpts<Encoding extends BufferEncoding, S extends BaseSchema<unknown>> = {
+  encoding: Encoding
+  parseAs: S
+}
+
+export type EnsureDefined<T> = T extends undefined ? never : T
 
 /**
  * Create a reference to another file.
  */
-export function file<Encoding extends (BufferEncoding | undefined) = undefined>(encoding?: Encoding): RefSchema<EncodingToSchemaType<Encoding>> {
+export function file<ParseAs extends BaseSchema<unknown>, Encoding extends (BufferEncoding | undefined) = undefined>(opts?: Encoding | FileOpts<EnsureDefined<Encoding>, ParseAs>): RefSchema<EncodingToSchemaType<Encoding>> {
+  const encoding = typeof opts === "object"
+    ? opts.encoding
+    : opts
+
+  const parseAs = typeof opts === "object"
+    ? opts.parseAs
+    : undefined
+
   return ref({
     schema: any<EncodingToSchemaType<Encoding>>(),
     sourceName: 'file',
-    refToSourceParams: (path: string) => ({file: path, encoding})
+    refToSourceParams: (path: string) => ({
+      file: path,
+      encoding,
+      ...parseAs !== undefined && { parseAs }
+    })
   })
 }
--- src/sources/file/schemes.unit.ts
+++ src/sources/file/schemes.unit.ts
@@ -3,6 +3,8 @@ import {expect} from "expect";
 import {file} from "./schemes.js";
 import {expectTypeOf} from "expect-type";
 import {kType} from "../../schemes/base.js";
+import {object} from "../../schemes/object.js";
+import {string} from "../../schemes/string.js";
 
 describe('schemes', function () {
   describe('file', function () {
@@ -44,6 +46,22 @@ describe('schemes', function () {
 
       // Then
       expectTypeOf(schema[kType]).toEqualTypeOf<string>()
+    })
+
+    it('should be able to parse the file content', function () {
+      // Given
+      // When
+      const schema = file({
+        encoding: 'utf8',
+        parseAs: object({ foo: string() })
+      })
+
+      // Then
+      expect(schema.refToSourceParams('file.txt')).toEqual(expect.objectContaining({
+        file: 'file.txt',
+        encoding: 'utf8',
+        parseAs: object({ foo: string() })
+      }))
     })
   })
 })
