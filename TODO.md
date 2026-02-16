19e9ca64815389fa5f1028da6f69c9128b461eb6
botflux <victor.mendele68@gmail.com>
2026-02-16 23:08:44 +0100

    basic user acceptance test

---
+++ TODO.md
@@ -0,0 +1,6 @@
+1. loading config from envs
+2. validating config
+3. developing basic schema like number, string, boolean, array, object
+4. support loading a file
+5. support loading a secret from a file 
+6. support loading a secret from vault
--- src/tests/basic.uat.ts
+++ src/tests/basic.uat.ts
@@ -1,10 +1,39 @@
 import { describe, expect, it } from "vitest";
 
-describe("basic features", () => {
-	it("should be able to work", () => {
+function object(param: { port: any }) {}
+
+function number(param: any[]) {}
+
+function envSource() {
+	return {
+		alias(name: string) {
+			return name;
+		},
+	};
+}
+
+async function load(
+	schema: void,
+	param2: { inject: { envs: { PORT: string } } },
+) {}
+
+describe("load config from envs", () => {
+	it.skip("should load a number from an env variable", async () => {
 		// Given
+		const envs = envSource();
+
+		const schema = object({
+			port: number([envs.alias("PORT")]),
+		});
+
 		// When
+		const config = await load(schema, {
+			inject: {
+				envs: { PORT: "3000" },
+			},
+		});
+
 		// Then
-		expect("foo").toEqual("foo");
+		expect(config).toEqual({ port: 3000 });
 	});
 });
