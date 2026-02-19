37a8f87ba399c584720e01aadd12164dbf13fefa
botflux <victor.mendele68@gmail.com>
2026-02-19 12:08:57 +0100

    exclude stryker from vitest targetted files

--- .config/vitest/vitest.config.ts
+++ .config/vitest/vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
 		environment: "node",
-		exclude: [ ".stryker-tmp" ],
+		exclude: [ ".stryker-tmp/**" ],
 		projects: [
 			{
 				extends: true,
